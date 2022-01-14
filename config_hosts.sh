2d96cb939733292a1abf103caff3dc53788bb801
ali-tm-amin <altmamin@gmail.com>
2022-01-14 20:45:26 +0000

    env var

--- config_hosts.sh
+++ config_hosts.sh
@@ -1,2 +1,10 @@
-echo "[app]\n$(terraform output app_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
-echo "[db]\n$(terraform output db_ip) ansible_user=ubuntu"  | sed 's/"//g' >> hosts.inv
+[15:48] Rajdeep Johal
+    #!/bin/bash
+echo -e "[app]\nec2-instance ansible_host=$(terraform output app_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
+echo -e "[db]\nec2-instance ansible_host=$(terraform output db_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
+
+output"app_instance_ip" {​​​value=aws_instance.app_instance.public_ip}​​​
+output"db_instance_ip" {​​​value=aws_instance.db_instance.public_ip}
+
+# echo "[app]\n$(terraform output app_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
+# echo "[db]\n$(terraform output db_ip) ansible_user=ubuntu"  | sed 's/"//g' >> hosts.inv
