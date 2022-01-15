76d9e4d504a4d892e123155f1bf8926881181d14
ali-tm-amin <altmamin@gmail.com>
2022-01-15 19:50:34 +0000

    updated ips

--- config_hosts.sh
+++ config_hosts.sh
@@ -2,8 +2,8 @@
 echo -e "[app]\nec2-instance ansible_host=$(terraform output app_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
 echo -e "[db]\nec2-instance ansible_host=$(terraform output db_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
 
-output"app_instance_ip" {​​​value=aws_instance.app_instance.public_ip}​​​
-output"db_instance_ip" {​​​value=aws_instance.db_instance.public_ip}
+output"app_ip" {value=aws_instance.app_instance.public_ip}
+output"db_ip" {value=aws_instance.db_instance.public_ip}
 
 # echo "[app]\n$(terraform output app_ip) ansible_user=ubuntu" | sed 's/"//g' > hosts.inv
 # echo "[db]\n$(terraform output db_ip) ansible_user=ubuntu"  | sed 's/"//g' >> hosts.inv
